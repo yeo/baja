e91ff5eb7fe4e1ee70f66b0e6b1de0d4cf2db302
Vinh <vinh@axcoto.com>
2019-04-29 02:31:53 -0700

    Consolidate

--- build.go
+++ build.go
@@ -1,8 +1,12 @@
 package baja
 
 import (
+	"bufio"
+	"fmt"
+	"html/template"
 	"os"
 	"path/filepath"
+	"sort"
 
 	"github.com/fatih/color"
 	"github.com/yeo/baja/utils"
@@ -74,4 +78,55 @@ func BuildDB(config *Config) *NodeDB {
 	color.Green("Scan content")
 	_ = filepath.Walk("./content", visit(db))
 	return db
+}
+
+func BuildIndex(dir string, nodes []*Node, current *Current) {
+	theme := GetTheme(DefaultConfig())
+
+	targetDirectory := "public/" + dir
+	os.MkdirAll(targetDirectory, os.ModePerm)
+
+	f, err := os.Create(targetDirectory + "/index.html")
+	if err != nil {
+		fmt.Println("Cannot create index.html in", targetDirectory, ". error", err)
+	}
+
+	w := bufio.NewWriter(f)
+
+	sort.Slice(nodes, func(i, j int) bool { return nodes[i].Meta.Date.After(nodes[j].Meta.Date) })
+	nodeData := make([]map[string]interface{}, len(nodes))
+
+	for i, n := range nodes {
+		nodeData[i] = n.data()
+	}
+
+	data := ListPage{
+		current,
+		dir,
+		dir,
+		nodeData,
+	}
+
+	tpl, err := template.New("layout").Funcs(FuncMaps()).ParseFiles(theme.LayoutPath("default"))
+	tpl, err = tpl.ParseFiles(theme.NodePath("index"))
+
+	if _, err := os.Stat(theme.Path() + dir + "/index.html"); err == nil {
+		tpl, err = tpl.ParseFiles(theme.Path() + dir + "/index.html")
+	}
+
+	if current.IsHome {
+		if _, err := os.Stat(theme.NodePath("home")); err == nil {
+			tpl, err = tpl.ParseFiles(theme.NodePath("home"))
+		}
+	}
+
+	if tpl == nil {
+		fmt.Println("Cannot create template render")
+		return
+	}
+
+	if err := tpl.Execute(w, data); err != nil {
+		fmt.Println("Fail to render. Check your template for syntax, wrong tag", err)
+	}
+	w.Flush()
 }

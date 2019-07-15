166ea89dbbf1e558c384e83f303affd66ee428e7
Vinh <vinh@axcoto.com>
2019-07-15 01:15:28 -0700

    Migrate site to context

--- context.go
+++ context.go
@@ -1,3 +1,17 @@
 package baja
 
+import (
+	"time"
+)
+
 type Context struct{}
+
+// Current is a struct about various current state we pass to template to help us do some business logic depend on a context
+type Current struct {
+	IsHome bool
+	IsDir  bool
+	IsTag  bool
+	IsList bool
+
+	CompiledAt time.Time
+}
